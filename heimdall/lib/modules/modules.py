90e91d87125931362d3727bc821e9978f59e8dac
Jonathan Becker <64037729+Jon-Becker@users.noreply.github.com>
2022-05-22 18:17:49 -0400

    Dist

--- heimdall/lib/modules/modules.py
+++ heimdall/lib/modules/modules.py
@@ -1,14 +1,18 @@
+from hashlib import md5
+import json
 import os
 import importlib
 import pathlib
-from struct import pack
 import traceback
+
+from heimdall.lib.utils.io import write
 
 from ..utils.colors import colorLib
 from ..utils.logger import log, logTraceback
 
 def getModules(args=None):
   mods = []
+  dist = []
   max_title_length = 0
   max_description_length = 0
   for item in os.listdir(pathlib.Path(__file__).parent.resolve()):
@@ -22,7 +26,13 @@ def getModules(args=None):
         if len(meta['description']) >= max_description_length:
           max_description_length = len(meta['description'])
         mods.append(meta)
+        dist.append({
+          "name": meta['title'],
+          "version": meta['version'],
+          "checksum": md5(open(f'{pathlib.Path(__file__).parent.resolve()}/{item}' ,'rb').read()).hexdigest()
+        })
       except Exception as e:
         log('warning', f'Module {colorLib.YELLOW}{item}{colorLib.RESET} failed to mount!')
         logTraceback(traceback.format_exc(), not args.verbose)
+  write(f'{pathlib.Path(__file__).parent.resolve()}/dist.json', json.dumps(dist, indent=4))
   return (mods, max_title_length, max_description_length)
