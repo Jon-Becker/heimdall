35b02e2f731ba6e8bf13cdad83596b5dc5ed2f4c
Jonathan Becker <64037729+Jon-Becker@users.noreply.github.com>
2022-05-29 20:26:04 -0400

    Release 1.0.3rc4

--- heimdall/lib/utils/logger.py
+++ heimdall/lib/utils/logger.py
@@ -11,12 +11,14 @@ logfile = f'{pathlib.Path(__file__).parent.parent.parent.resolve()}/logs/heimdal
 
 # purge old log files
 def purgeOldLogfiles():
-  files = os.listdir(f'{pathlib.Path(__file__).parent.parent.parent.resolve()}/logs')
-  files.remove('__init__.py')
-  for file in files:
-      if int(file.split('-')[1].replace('.log', '')) < int(datetime.datetime.now().strftime("%Y%m%d%H%M%S")) - 100000:
-        os.remove(f'{pathlib.Path(__file__).parent.parent.parent.resolve()}/logs/{file}')
-    # if timestamp is over 1 day old
+  try:
+    files = os.listdir(f'{pathlib.Path(__file__).parent.parent.parent.resolve()}/logs')
+    files.remove('__init__.py')
+    for file in files:
+        if int(file.split('-')[1].replace('.log', '')) < int(datetime.datetime.now().strftime("%Y%m%d%H%M%S")) - 100000:
+          os.remove(f'{pathlib.Path(__file__).parent.parent.parent.resolve()}/logs/{file}')
+      # if timestamp is over 1 day old
+  except: pass
       
 # log to file, removing color codes
 def logStripColor(file, type, log):
